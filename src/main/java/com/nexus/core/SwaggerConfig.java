282a24f74ce55d65c815ecb7299423661d2e6415
crysthian-pires <crysthian.mello@gmail.com>
2026-06-26 02:55:17 -0300

    feat(docs): adicionar Swagger e PingController

---
+++ src/main/java/com/nexus/core/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.nexus.core;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Nexus API")
+                        .version("1.0.0")
+                        .description("Documentação da API do Nexus"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Token"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Token", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
